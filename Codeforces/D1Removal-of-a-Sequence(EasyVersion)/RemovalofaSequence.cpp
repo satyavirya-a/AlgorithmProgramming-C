7995dc45c1340f668c3cf5649754fcd19f1157a0
Satya Virya Atmadja <satyaatmadja07@gmail.com>
2025-12-14 17:41:23 +0700

    Add Removable Sequence

---
+++ Codeforces/D1Removal-of-a-Sequence(EasyVersion)/RemovalofaSequence.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int t; 
+
+    scanf("%d", &t);
+
+    for (int i =0 ; i < t; i++) {
+        int x, y, k;
+        scanf("%d %d %d", &x, &y, &k);
+
+        for (int j = 0; j < )
+    }
+
+
+    return 0;
+}
